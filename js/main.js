d0d62510e6ebb2607ee58382367c15cf54b04fb3
DatHa09 <hathucdat4@gmail.com>
2022-09-23 03:01:10 +0700

    responsive tablet, mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+const header = document.getElementById("header");
+
+const mobile_menu = document.querySelector(".mobile_menu_btn");
+
 const slider_img = document.getElementById("image");
 
 const get_heading_content = document.querySelector(".text_heading");
+
 const get_desc_content = document.querySelector(".text_desc");
 
 const buy_Btns = document.querySelectorAll(".js_buy_ticket");
@@ -34,6 +39,7 @@ slider_img.src = imageArr[0].url;
 get_heading_content.innerText = imageArr[0].heading;
 get_desc_content.innerText = imageArr[0].descriptions;
 
+// show list image and content
 let index = 1;
 function autoSlideShowImage() {
   slider_img.src = imageArr[index].url;
@@ -48,6 +54,7 @@ function autoSlideShowImage() {
 
 setInterval(autoSlideShowImage, 5000);
 
+// open / close modal when clicked buy ticket button
 for (let buyBtn of buy_Btns) {
   buyBtn.onclick = (e) => {
     modal.classList.add("open");
@@ -66,3 +73,23 @@ modal.onclick = () => {
 modal_container.onclick = (e) => {
   e.stopPropagation();
 };
+
+//toggle mobile menu
+const current_header_height = header.clientHeight;
+mobile_menu.onclick = (e) => {
+  let isClosed = header.clientHeight === current_header_height;
+  isClosed ? (header.style.height = "auto") : (header.style.height = null);
+};
+
+//auto close menu when clicked menu item
+let menuItems = document.querySelectorAll("#nav li a[href*='#']");
+for (let i = 0; i < menuItems.length; i++) {
+  let menuItem = menuItems[i];
+  menuItem.onclick = (e) => {
+    let isParentMenu =
+      e.target.nextElementSibling &&
+      e.target.nextElementSibling.classList.contains("sub_nav");
+
+    isParentMenu ? e.preventDefault() : (header.style.height = null);
+  };
+}
